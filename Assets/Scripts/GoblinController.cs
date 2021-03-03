839280cb765977094dc28aec791cfedb38ab50b0
dstuck <david.e.stuck@gmail.com>
2021-03-02 21:16:42 -0600

    Add stab animation

--- Assets/Scripts/GoblinController.cs
+++ Assets/Scripts/GoblinController.cs
@@ -54,10 +54,10 @@ public class GoblinController : MonoBehaviour
                 isInvincible = false;
         }
 
-        //if (Input.GetKeyDown(KeyCode.C))
-        //{
-        //    Launch();
-        //}
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Attack();
+        }
     }
 
     void FixedUpdate()
@@ -69,15 +69,10 @@ public class GoblinController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    //void Launch()
-    //{
-    //    GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
-
-    //    Projectile projectile = projectileObject.GetComponent<Projectile>();
-    //    projectile.Launch(lookDirection, 300);
-
-    //    animator.SetTrigger("Launch");
-    //}
+    void Attack()
+    {
+        animator.SetTrigger("Stab");
+    }
 
     public void ChangeHealth(int amount)
     {
