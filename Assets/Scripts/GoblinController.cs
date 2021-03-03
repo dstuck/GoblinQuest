90aa0d31768eef51f535e7f7c2d7c446d849213c
dstuck <david.e.stuck@gmail.com>
2021-03-02 21:02:21 -0600

    Initial state, animated walking goblin

---
+++ Assets/Scripts/GoblinController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        //animator.SetFloat("Look X", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    Launch();
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    //void Launch()
+    //{
+    //    GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+    //    Projectile projectile = projectileObject.GetComponent<Projectile>();
+    //    projectile.Launch(lookDirection, 300);
+
+    //    animator.SetTrigger("Launch");
+    //}
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            //animator.SetTrigger("Hit");
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+My second unity project to create a beat em up about a weak little goblin
+
+Using Unity 2020.1.4f1
+
+Local Testing
+=============
+From the Build directory, start a server: `python -m http.server --cgi 8360`
+Then access the server via: `http://localhost:8360/index.html`
+
+Release
+=======
+`butler push Builds/GoblinQuest.zip drawacard/GoblinQuest/:webgl-beta`
+
+Acknowledgments
+===============
+Thanks to @Calciumtrice for the sprites https://opengameart.org/content/animated-goblins
